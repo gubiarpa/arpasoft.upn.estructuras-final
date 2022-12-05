491318be23154d2fd12a0bd70f24e37fd94c0fb0
Billy Arredondo <billy.arredondo@pucp.pe>
2022-12-04 22:54:06 -0500

    Test 1

--- src/garredondo.evaluacion-final.console/Program.cs
+++ src/garredondo.evaluacion-final.console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using garredondo.evaluacion_final.console.Data;
+using garredondo.evaluacion_final.console.Matrix;
+using garredondo.evaluacion_final.core.Entities;
+
+var matrix = (new DataLoader()).GetMap8x8();
+
+var matrixTreeNodeService = new MatrixTreeNodeService(matrix);
+
+var cellStart = new Cell(1, 2);
+var cellEnd = new Cell(7, 7);
+
+var paths = matrixTreeNodeService.GetPaths(cellStart, cellEnd);
